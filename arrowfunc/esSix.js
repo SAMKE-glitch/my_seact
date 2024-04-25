55423687c0c4a34a5d5515c0e9d6895e9649f678
samke-glitch <sammythemwa@gmail.com>
2024-04-25 12:18:28 +0300

    arrowfunc with param

--- arrowfunc/esSix.js
+++ arrowfunc/esSix.js
@@ -2,3 +2,11 @@
 const sayHello = () => console.log("Es6 function - Hello World")
 // Arrow function can be passed as parameters for callbacks
 setTimeout(sayHello, 1000);
+
+//arrow function with parameters
+const oneParamArroFunc = name => {return "Hello" + name};
+const twoParamsArrowFuncWithoutReturn = (first, last) => console.log("hello" + first + " "+ last);
+const twoParamsTwoLinesArrowFunc = (first, last) => {
+  const greeting = "hello ";
+	return greeting+" "+first + " " + last;
+}
