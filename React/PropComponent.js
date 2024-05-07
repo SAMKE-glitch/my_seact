5e08632a88a064f816dde5c5a7981b5aedfc3a26
samke-glitch <sammythemwa@gmail.com>
2024-05-07 12:57:28 +0300

    edit props

--- React/PropComponent.js
+++ React/PropComponent.js
@@ -3,6 +3,7 @@ class TestComponent extends React.component {
     render() {
         return <div> Hi {this.props.name}</div>
     }
+// passing the props as example to the test component
 <TestComponent name='John'/>
 <TestComponent name='Jill'/>
 }
